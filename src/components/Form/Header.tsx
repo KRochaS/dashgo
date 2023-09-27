e1d224b2547c46b3ae40c1d8c96cdb407e64a3db
KRochaS <karinerocha.s@hotmail.com>
2023-09-27 20:50:33 -0300

    Header Component

--- src/components/Form/Header.tsx
+++ src/components/Form/Header.tsx
@@ -1,6 +1,7 @@
-import { Flex } from '@chakra-ui/react'
+'use client';
 
-
+import { Avatar, Box, Flex, HStack, Icon, Input, Text } from '@chakra-ui/react';
+import { RiNotification2Line, RiSearchLine, RiUserAddLine } from 'react-icons/ri';
 export function Header() {
     return (
         <Flex
@@ -14,7 +15,78 @@ export function Header() {
             align="center"
 
         >
-            <h1> Dashgo</h1>
+
+            <Text
+                fontSize="3xl"
+                fontWeight="bold"
+                letterSpacing="tight"
+                w="64"
+            >
+                dashgo
+                <Text
+                    ml="1"
+                    color="pink.500"
+                    as="span">
+                    .
+                </Text>
+            </Text>
+
+            <Flex
+                as="label"
+                flex="1"
+                py="4"
+                px="8"
+                ml="6"
+                maxWidth={400}
+                alignSelf="center"
+                color="gray.200"
+                position="relative"
+                bg="gray.800"
+                borderRadius="full"
+            >
+
+                <Input
+                    color="gray.50"
+                    variant="unstyled"
+                    placeholder="Search"
+                    _placeholder={{ color: 'gray.400' }}
+                    px="4"
+                    mr="4"
+                />
+
+                <Icon as={RiSearchLine} fontSize="20" />
+            </Flex>
+
+
+            <Flex align="center" ml="auto">
+
+                <HStack spacing="8"
+                 mx="8"
+                 pr="8"
+                 py="1"
+                 color="gray.300"
+                 borderRightWidth={1}
+                 borderColor="gray.700"
+               >
+
+                <Icon as={RiNotification2Line} fontSize="20" />
+                <Icon as={RiUserAddLine} fontSize="20" />
+                </HStack>
+            </Flex>
+
+            <Flex align="center">
+                <Box mr="4" textAlign="right">
+                    <Text> Karine Rocha </Text>
+                    <Text
+                      color="gray.300"
+                      fontSize="small"
+                    > 
+                      karinerocha.s@hotmail.com 
+                    </Text>
+                </Box>
+
+                <Avatar size="md" name="Karine Rocha" src="https://github.com/KRochaS.png"/>
+            </Flex>
 
         </Flex>
     )
