4c0d51d6ae3eba00d42f6575ffa17a74d3ce3999
KRochaS <karinerocha.s@hotmail.com>
2023-10-04 20:31:14 -0300

    Sidebar component

---
+++ src/components/Sidebar/SidebarNav.tsx
@@ -0,0 +1,30 @@
+import { Stack } from "@chakra-ui/react";
+import { RiContactsLine, RiDashboardLine, RiGithubLine, RiInputMethodLine } from "react-icons/ri";
+import { NavLink } from "./NavLink";
+import { NavSection } from "./NavSection";
+
+export function SidebarNav() {
+    return (
+        <Stack spacing="12" align="flex-start">
+            <NavSection title="GERAL">
+                <NavLink icon={RiDashboardLine}>
+                    Dashboad
+                </NavLink>
+
+                <NavLink icon={RiContactsLine}>
+                    Usuários
+                </NavLink>
+            </NavSection>
+
+            <NavSection title="AUTOMAÇÃO">
+                <NavLink icon={RiInputMethodLine}>
+                    Formulários
+                </NavLink>
+
+                <NavLink icon={RiGithubLine}>
+                    Automação
+                </NavLink>
+            </NavSection>
+        </Stack>
+    )
+}
