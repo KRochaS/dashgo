dcc398f42ef6fa019a685dde65b1268beb94b2a0
KRochaS <karinerocha.s@hotmail.com>
2023-10-12 14:28:01 -0300

    Sidebar

--- src/components/Sidebar/SidebarNav.tsx
+++ src/components/Sidebar/SidebarNav.tsx
@@ -7,21 +7,21 @@ export function SidebarNav() {
     return (
         <Stack spacing="12" align="flex-start">
             <NavSection title="GERAL">
-                <NavLink icon={RiDashboardLine}>
+                <NavLink icon={RiDashboardLine} href="/dashboard">
                     Dashboad
                 </NavLink>
 
-                <NavLink icon={RiContactsLine}>
+                <NavLink icon={RiContactsLine} href="/users">
                     Usuários
                 </NavLink>
             </NavSection>
 
             <NavSection title="AUTOMAÇÃO">
-                <NavLink icon={RiInputMethodLine}>
+                <NavLink icon={RiInputMethodLine} href="/forms">
                     Formulários
                 </NavLink>
 
-                <NavLink icon={RiGithubLine}>
+                <NavLink icon={RiGithubLine} href="/automation">
                     Automação
                 </NavLink>
             </NavSection>
