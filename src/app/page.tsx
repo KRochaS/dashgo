a81ccc6035af0edd3bb5cbb0f1eb5f862c61c34c
KRochaS <karinerocha.s@hotmail.com>
2023-10-14 19:51:35 -0300

    Login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,35 @@
+'use client';
 
 import { Input } from "@/components/Form/Input";
 import { Button, Flex, Stack } from "@chakra-ui/react";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useForm } from "react-hook-form";
+import * as yup from 'yup';
+
+type SignInFormData = {
+    email: string;
+    password: string;
+}
+
+const signInFormSchema = yup.object().shape({
+    email: yup.string().required('E-mail obrigatório').email('E-mail inválido'),
+    password: yup.string().required('Senha obrigatória'),
+})
+
 
 export default function SignIn() {
+    const { register, handleSubmit, formState } = useForm({
+        resolver: yupResolver(signInFormSchema)
+    });
+
+    const { isSubmitting, errors } = formState;
+
+
+    async function handleSignIn(values: any) {
+        await new Promise(resolve => setTimeout(resolve, 2000)); 
+        console.log(values);
+    }
+    
     return (
         <Flex
             w="100%"
@@ -19,12 +46,22 @@ export default function SignIn() {
                 p="8"
                 borderRadius="8"
                 flexDir="column"
+                onSubmit={handleSubmit(handleSignIn)}
 
             >
 
                 <Stack spacing="4">
-                    <Input name="email" label="E-mail" type="email" />
-                    <Input name="password" label="Password" type="password" />
+                    <Input label="E-mail" 
+                    type="email" 
+                    error={errors?.email}
+                    {...register('email')} 
+                   
+                    />
+                    <Input 
+                        label="Password" 
+                        type="password" 
+                        {...register('password')} 
+                        error={errors.password} />
                 </Stack>
 
                 <Button
@@ -32,6 +69,7 @@ export default function SignIn() {
                     mt="6"
                     colorScheme="pink"
                     size="lg"
+                    isLoading={isSubmitting}
                 >
                     Enter
                 </Button>
